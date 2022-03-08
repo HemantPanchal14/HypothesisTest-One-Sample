da3fd4a38c83cd59315bdbbbc9561d7a9008c657
Hemant Panchal <96882837+HemantPanchal14@users.noreply.github.com>
2022-03-09 00:05:30 +0530

    Add files via upload

---
+++ HypoTest-oneSample.R
@@ -0,0 +1,43 @@
+# one-sample 't'-test
+#it's used to determine whether an Unknown Popullation MEAN is different from a specific value
+
+#CAse study: An outbreak of salmonella related illness was attributed to ice cream produced at a certain factory
+#scientist measured the level of salmonella in 9 randomly sampled batches of ice cream.
+#The level in MPN/g were
+
+# 0.593,0.142,0.329,0.691,0.231,0.793,0.519,0.392,0.418
+# Is there evidence that the mean level of salmonella in the ice cream is greater than 0.3 mpn/g!
+# we have only ONE sample dat with 9 nos. of observations
+
+#Solution:
+
+#Ho Null hypothesis= mean level is less then equal to 0.3
+#Ha Alternate hypothesis= mean level is GREATER than 0.3
+  
+?t.test
+
+#t.test(x,alternative=,mu),where x= All 9 nos. of data all together
+# will use 'vector' as 'c' for all above readings ( it will hold all values)
+
+x<-c (0.593,0.142,0.329,0.691,0.231,0.793,0.519,0.392,0.418)
+ # assigned as 'x'
+x      
+ # will apply t.test formula for One-sample test
+
+t.test(x,alternative = "greater",mu=0.3)
+
+#ans:data:  x
+# t = 2.2051, df = 8, p-value = 0.02927
+# alternative hypothesis: true mean is greater than 0.3
+# 95 percent confidence interval:
+# 0.3245133       Inf
+# sample estimates:
+#mean of x 
+# 0.4564444 
+
+#0.3  MPN/g is our Mu 'thresh hold'
+
+# now, p value compare to risk factor'alfa' 0.05
+# Here,we got p-value = 0.02927 < alfa 0.05.hence, we reject Ho null hypothesis and accept
+       
+#Ha - Alternate hypothesis= mean level is GREATER than 0.3
