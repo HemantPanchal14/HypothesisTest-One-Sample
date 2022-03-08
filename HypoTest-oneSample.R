307a60b6c30e5eaf2574dda343c45fcc1f0ea5f2
Hemant Panchal <96882837+HemantPanchal14@users.noreply.github.com>
2022-03-09 00:06:01 +0530

    Update HypoTest-oneSample.R

--- HypoTest-oneSample.R
+++ HypoTest-oneSample.R
@@ -1,13 +1,4 @@
-# one-sample 't'-test
-#it's used to determine whether an Unknown Popullation MEAN is different from a specific value
 
-#CAse study: An outbreak of salmonella related illness was attributed to ice cream produced at a certain factory
-#scientist measured the level of salmonella in 9 randomly sampled batches of ice cream.
-#The level in MPN/g were
-
-# 0.593,0.142,0.329,0.691,0.231,0.793,0.519,0.392,0.418
-# Is there evidence that the mean level of salmonella in the ice cream is greater than 0.3 mpn/g!
-# we have only ONE sample dat with 9 nos. of observations
 
 #Solution:
 
